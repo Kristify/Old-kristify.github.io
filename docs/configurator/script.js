5839ee9a322cb439c19f07fb2ed0d75ea809a599
Erlend <49862976+Erb3@users.noreply.github.com>
2022-12-25 21:00:53 +0100

    feat: Create generator (part 2)

---
+++ docs/configurator/script.js
@@ -0,0 +1,67 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+
+createApp({
+  data() {
+    return {
+      pkey: '',
+      name: '',
+      self: '',
+      storage: [],
+      storageFields: 1,
+      noMetaname: '',
+      nonexistantItem: '',
+      notEnogthMoney: '',
+      notEnogthStock: '',
+      change: '',
+      startedSound: '',
+      purchaseSound: '',
+      errorSound: '',
+      webhookType: '1',
+      webhook: '',
+      result: '',
+    };
+  },
+
+  methods: {
+    getStorage() {
+      const lines = [];
+
+      for (let chest of this.storage) {
+        lines.push(`      "${chest}",`);
+      }
+
+      return lines.join('\n');
+    },
+
+    generate() {
+      this.result = `
+return {
+  pkey = "${this.pkey}",
+  name = "${this.name}",
+  storage = {
+${this.getStorage()}
+  },
+  self = "${this.self}",
+  messages = {
+    noMetaname      = "${this.noMetaname}",
+    nonexistantItem = "${this.nonexistantItem}",
+    notEnogthMoney  = "${this.notEnogthMoney}",
+    notEnogthStock  = "${this.notEnogthStock}",
+    change          = "${this.change}"
+  },
+  webhooks = {
+    {
+      type = "${this.webhookType}",
+      url = "${this.webhook}"
+    }
+  },
+  sounds = {
+    started = "${this.startedSound}",
+    purchase = "${this.purchaseSound}",
+    error = "${this.errorSound}"
+  }
+}
+      `;
+    },
+  },
+}).mount('#main');
